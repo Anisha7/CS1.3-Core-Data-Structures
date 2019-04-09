b9e1a1d34e0b3e36ca2cd330e406435b7252cd70
Anisha Jain <anishajain@outlook.com>
2019-04-09 15:31:48 -0700

    debugging find all indexes

--- strings.py
+++ strings.py
@@ -63,12 +63,28 @@ def find_index(text, pattern):
     return None
 
 
-def find_all_indexes(text, pattern):
+def find_all_indexes(text, pattern, result = [], left=0):
     """Return a list of starting indexes of all occurrences of pattern in text,
     or an empty list if not found."""
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement find_all_indexes here (iteratively and/or recursively)
+    print(left)
+    if (pattern == ''):
+        return list(range(len(text)))
+    # make sure pattern exists
+    if (contains(text[left:], pattern) == False):
+        return result
+
+    if (len(text[left:]) <= 0 or left >= len(text)):
+        return result
+    # gets the first index for pattern in text
+    found_index = find_index(text[left:], pattern) + left
+    print("ADDING: %d"% found_index)
+    result.append(found_index)
+    # we didn't find it -->
+    left = found_index + len(pattern)
+    return find_all_indexes(text, pattern, result, left)
 
 
 def test_string_algorithms(text, pattern):
@@ -76,7 +92,6 @@ def test_string_algorithms(text, pattern):
     print('contains({!r}, {!r}) => {}'.format(text, pattern, found))
     # TODO: Uncomment these lines after you implement find_index
     index = find_index(text, pattern)
-    print(find_index(text, pattern))
     print('find_index({!r}, {!r}) => {}'.format(text, pattern, index))
     # TODO: Uncomment these lines after you implement find_all_indexes
     indexes = find_all_indexes(text, pattern)
