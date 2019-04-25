1d8aa237514b95836f64d50ce1dffeadb57a6437
Anisha Jain <anishajain@outlook.com>
2019-04-25 11:57:46 -0700

    refactoring bases

--- bases.py
+++ bases.py
@@ -11,167 +11,6 @@ import math
 # string.printable is digits + ascii_letters + punctuation + whitespace
 
 
-# ```````` helper functions for decode ```````` #
-
-# digit string to digits, base = 10
-def digit(digits):
-    return int(digits)
-
-# binary to digits, base = 2
-def binaryToDigits(digits):
-    mult = 0 # the exponential power for 2
-    result = 0
-
-    for i in range(len(digits)-1, -1, -1): # loop in reverse
-        if (digits[i] == ' '):
-            continue
-        temp = 0 # what binary digit we are on, 1 or 0
-        if (digits[i] == '1'):
-            temp = 1
-
-        result += (2**mult)*temp
-        mult += 1
-    
-    return result
-
-# hexadecimal to digits, base = 16
-def hexToDigits(digits):
-    # if digit: use that as temp
-    # if letter: 10 + (ord('letter') - ord('A'))
-    mult = 0
-    result = 0
-    
-    for i in range(len(digits)-1, -1, -1): # loop in reverse
-        curr = digits[i]
-        # print("curr:%s"%curr)
-        temp = 0 # value for current hex bit
-        # print("temp:%d"%temp)
-        if (curr == ' '): # if space
-            continue
-        elif (curr.lower() == 'x'): # if marker for hex, but not part of hex num
-            break
-        elif (curr.isdigit()): # if number
-            temp = int(curr)
-        elif (curr.isalpha()): # if letter
-            curr = curr.lower()
-            temp = 10 + (ord(curr) - ord('a')) # get its value
-        
-        result += (16**mult)*temp
-        # print(result, mult, temp)
-        mult += 1
-    
-    return result
-
-# all bases
-def convertToDigits(digits, base):
-    mult = 0 # exponent power for base
-    result = 0
-    
-    for i in range(len(digits)-1, -1, -1): # loop in reverse
-        curr = digits[i]
-        # print(curr)
-        temp = 0 # value for current hex bit
-        if (curr == ' '): # if space
-            continue
-        # elif (curr.lower() == 'x'): # if marker for hex, but not part of hex num
-        #     break
-        temp = string.printable.index(curr)
-        # temp = 10 + (ord(curr) - ord('a'))
-        result += (base**mult)*temp
-        mult += 1
-    
-    return result
-
-
-# ```````` helper functions for encode ```````` #
-def getBinaryLength(digits, i):
-    exp = int(math.log(digits, i))
-    # make sure exp is valid 
-    if (i == 2 and (exp+1)%4 != 0):
-        if ((exp+2)%4 == 0):
-            exp += 1
-        elif ((exp-2)%4 == 0):
-            exp -= 1
-    return exp
-# input is an integer, convert it to base 2
-def digitToBinary(digit):
-    result = ""
-    # to get the len for binary and max exp value for 2, do log base 2 of digit
-    # use that as the start value
-    exp = getBinaryLength(digit, 2)
-    # grammar, spacing
-    # count = 0
-    while (exp >= 0):
-        # formatting
-        # if (count == 4):
-        #     count = 0
-        #     result += ' '
-        
-        # 2's exponential value for this bit
-        temp = 2**exp
-        exp -= 1
-        if (digit >= temp):
-            digit -= temp
-            result += '1'
-        else:
-            result += '0'
-        # count += 1
-    return result
-
-# digit to hexadecimal, base 16
-def digitToHex(digit):
-    result = ""
-    hexvals = string.hexdigits
-    print(hexvals)
-    while (digit > 0):
-        rem = int(digit % 16)
-        print(rem)
-        # remainder is the hex digit
-        result = hexvals[rem] + result
-
-        digit //= 16
-        print("DIGIT: %d"%digit)
-        print(digit > 0)
-    return result
-
-# converts a digit to any base specified
-def digitToBase(digit, base):
-    # digit = int(digit)
-    result = ""
-    # print(hexvals)
-    while (digit > 0):
-        rem = int(digit % base) # the hex digit
-        result = string.printable[rem] + result
-        digit //= base
-    return result
-
-# ```````` helper functions for convert ```````` #
-
-# Convert digits from base 2 to base 16 (and vice versa)
-def convert2and16(digits, base):
-    result = ""
-    digits = digits.lower()
-    if base == 16:
-        # evaluate binary for each hex bit
-        for dig in digits:
-            temp = 0
-            if (dig.digit()):
-                temp = int(dig)
-            if (dig.isalpha()):
-                temp = 10 + (ord(dig) - ord('a')) # get its value
-            result += encode(temp, 2)
-    if base == 2:
-        # make sure length is in 4's to convert each set of 4 to binary
-        while (len(digits)%4 != 0):
-            digits = '0' + digits
-        # print(digits)
-
-        for i in range(0, len(digits), 4):
-            digit10 = decode(digits[i:i+4], 2) # base 10
-            result += encode(digit10, 16)
-    return result.lower()
-
-
 # ```````` ASSIGNED: DECODE AND ENCODE FUNCTIONS ```````` #
 
 def decode(digits, base):
@@ -374,3 +213,161 @@ if __name__ == '__main__':
     print(digitToTwosComplement(-100))
     print(digitToTwosComplement(-127))
 
+
+
+
+# ```````` helper functions previously implemented for decode for base specific conversions ```````` #
+
+# binary to digits, base = 2
+# def binaryToDigits(digits):
+#     mult = 0 # the exponential power for 2
+#     result = 0
+
+#     for i in range(len(digits)-1, -1, -1): # loop in reverse
+#         if (digits[i] == ' '):
+#             continue
+#         temp = 0 # what binary digit we are on, 1 or 0
+#         if (digits[i] == '1'):
+#             temp = 1
+
+#         result += (2**mult)*temp
+#         mult += 1
+    
+#     return result
+
+# # hexadecimal to digits, base = 16
+# def hexToDigits(digits):
+#     # if digit: use that as temp
+#     # if letter: 10 + (ord('letter') - ord('A'))
+#     mult = 0
+#     result = 0
+    
+#     for i in range(len(digits)-1, -1, -1): # loop in reverse
+#         curr = digits[i]
+#         # print("curr:%s"%curr)
+#         temp = 0 # value for current hex bit
+#         # print("temp:%d"%temp)
+#         if (curr == ' '): # if space
+#             continue
+#         elif (curr.lower() == 'x'): # if marker for hex, but not part of hex num
+#             break
+#         elif (curr.isdigit()): # if number
+#             temp = int(curr)
+#         elif (curr.isalpha()): # if letter
+#             curr = curr.lower()
+#             temp = 10 + (ord(curr) - ord('a')) # get its value
+        
+#         result += (16**mult)*temp
+#         # print(result, mult, temp)
+#         mult += 1
+    
+#     return result
+
+# # all bases
+# def convertToDigits(digits, base):
+#     mult = 0 # exponent power for base
+#     result = 0
+    
+#     for i in range(len(digits)-1, -1, -1): # loop in reverse
+#         curr = digits[i]
+#         # print(curr)
+#         temp = 0 # value for current hex bit
+#         if (curr == ' '): # if space
+#             continue
+#         # elif (curr.lower() == 'x'): # if marker for hex, but not part of hex num
+#         #     break
+#         temp = string.printable.index(curr)
+#         # temp = 10 + (ord(curr) - ord('a'))
+#         result += (base**mult)*temp
+#         mult += 1
+    
+#     return result
+
+
+# # ```````` helper functions for encode ```````` #
+# def getBinaryLength(digits, i):
+#     exp = int(math.log(digits, i))
+#     # make sure exp is valid 
+#     if (i == 2 and (exp+1)%4 != 0):
+#         if ((exp+2)%4 == 0):
+#             exp += 1
+#         elif ((exp-2)%4 == 0):
+#             exp -= 1
+#     return exp
+# # input is an integer, convert it to base 2
+# def digitToBinary(digit):
+#     result = ""
+#     # to get the len for binary and max exp value for 2, do log base 2 of digit
+#     # use that as the start value
+#     exp = getBinaryLength(digit, 2)
+#     # grammar, spacing
+#     # count = 0
+#     while (exp >= 0):
+#         # formatting
+#         # if (count == 4):
+#         #     count = 0
+#         #     result += ' '
+        
+#         # 2's exponential value for this bit
+#         temp = 2**exp
+#         exp -= 1
+#         if (digit >= temp):
+#             digit -= temp
+#             result += '1'
+#         else:
+#             result += '0'
+#         # count += 1
+#     return result
+
+# # digit to hexadecimal, base 16
+# def digitToHex(digit):
+#     result = ""
+#     hexvals = string.hexdigits
+#     print(hexvals)
+#     while (digit > 0):
+#         rem = int(digit % 16)
+#         print(rem)
+#         # remainder is the hex digit
+#         result = hexvals[rem] + result
+
+#         digit //= 16
+#         print("DIGIT: %d"%digit)
+#         print(digit > 0)
+#     return result
+
+# # converts a digit to any base specified
+# def digitToBase(digit, base):
+#     # digit = int(digit)
+#     result = ""
+#     # print(hexvals)
+#     while (digit > 0):
+#         rem = int(digit % base) # the hex digit
+#         result = string.printable[rem] + result
+#         digit //= base
+#     return result
+
+# # ```````` helper functions for convert ```````` #
+
+# # Convert digits from base 2 to base 16 (and vice versa)
+# def convert2and16(digits, base):
+#     result = ""
+#     digits = digits.lower()
+#     if base == 16:
+#         # evaluate binary for each hex bit
+#         for dig in digits:
+#             temp = 0
+#             if (dig.digit()):
+#                 temp = int(dig)
+#             if (dig.isalpha()):
+#                 temp = 10 + (ord(dig) - ord('a')) # get its value
+#             result += encode(temp, 2)
+#     if base == 2:
+#         # make sure length is in 4's to convert each set of 4 to binary
+#         while (len(digits)%4 != 0):
+#             digits = '0' + digits
+#         # print(digits)
+
+#         for i in range(0, len(digits), 4):
+#             digit10 = decode(digits[i:i+4], 2) # base 10
+#             result += encode(digit10, 16)
+#     return result.lower()
